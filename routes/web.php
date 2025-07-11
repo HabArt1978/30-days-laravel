5c67255bbd37ba4abb3ef12b1b29b45539f64af9
HabArt <105586654+HabArt1978@users.noreply.github.com>
2025-07-11 13:00:17 +0300

    6 - use of external data within the function

--- routes/web.php
+++ routes/web.php
@@ -2,51 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '$50,000'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary' => '$10,000'
+    ],
+    [
+        'id' => 3,
+        'title' => 'Teacher',
+        'salary' => '$40,000'
+    ],
+];
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ],
-        ]
-
+        'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ],
-    ];
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
+
     // Реализация без стрелочной функции
     // Arr::first($jobs, function ($job) use ($id) {
     //     return $job['id'] === $id;
