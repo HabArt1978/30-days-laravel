3d9a1735f65ed59e565f047caf9084eb19ba0103
HabArt <105586654+HabArt1978@users.noreply.github.com>
2025-08-12 02:26:23 +0300

    - changed the color of the links in the header

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 <?php
 $classes = ['rounded-md px-3 py-2 text-sm font-medium'];
-$classes[] = $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
+$classes[] = $active ? 'bg-gray-700 text-white' : 'text-gray-300 hover:text-white';
 ?>
 
 <a class="{{ join(' ', $classes) }}"
